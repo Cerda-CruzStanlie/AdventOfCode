331d627607ba71c67600f05c3d67a42694057a03
Cerda-CruzStanlie <115739287+Cerda-CruzStanlie@users.noreply.github.com>
2022-12-22 18:21:53 -0500

    Final

--- day1.py
+++ day1.py
@@ -1,4 +1,39 @@
+# Advent Of Code: Author Stanlie Cerda-Cruz
+# Code Inspiration: Jefffery Frederic
+
 with open('day1.in') as file:
     data = [i for i in file.read().strip().split('\n')]
 
-print(data)
+# Traversing every STRING in our DATA
+x = 0
+i = 0
+calories = 0
+count = 0
+elves = []
+for item in data:
+    x = x + 1
+    if item == '':
+        count = count + 1
+        elves.append(count)
+        elves.append(calories)
+        calories = 0
+    else:
+        calories = calories + int(item)
+    if len(data) == x:
+        count = count + 1
+        elves.append(count)
+        elves.append(calories)
+
+print(elves)
+
+#Max Calories
+max = max(elves)
+print(max)
+
+#Removes elve count then organizes
+for item in elves:
+    if item <= count:
+        elves.remove(item)
+elves.sort(*)
+print(elves)
+
